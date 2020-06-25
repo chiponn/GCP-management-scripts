af2cf9e9ada3527afed5ed42849c83e5e7d5e9a3
Yonatan Brand <chiponn@gmail.com>
2020-06-25 11:13:49 +0300

    Update gcpBucket.sh

--- scripts/gcpBucket.sh
+++ scripts/gcpBucket.sh
@@ -3,21 +3,22 @@
 # Script designed to delete all instances that were not used in the last 20 days.          |         Designed by Yonatan Brand
 ##############################################################################################################################
 
-export GCE_PROJECT=support-prod-157422
+export GCE_PROJECT=
 
 #############################################################
 
 /snap/bin/gsutil ls > /home/yonatanb/gcpOperator/logs/bucketList.txt
 input="/home/yonatanb/gcpOperator/logs/bucketList.txt"
 
-LBUCKET="gs://loren-bucket/"
+# LBUCKET is for a large bucket if we want to avoid GCP to calculate it's size
+LBUCKET=
 
     while IFS= read -r line
     do
         echo "$line"
 	if [ "$line" = "$LBUCKET" ];
 		then
-			echo -e "Loren's bucket. Skipping \n"
+			echo -e "Large Bucket. Skipping \n"
 	else 
 		RESULT=`/snap/bin/gsutil du -s $line | awk '{print $1}'`
 		if [ "$RESULT" = "0" ];
